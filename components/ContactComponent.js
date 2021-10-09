87f0f27b70d17c4c78bb30b2b46357efa45baf5e
hugosmoreira <hugo.n4@gmail.com>
2021-10-08 18:29:48 -0700

    update nucampwebsite redux

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,37 +1,31 @@
-import React, { Component } from 'react'
-
-import { Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements'
+import React, { Component } from 'react';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
-    constructor(props) {
-        super(props);
-      
-    }
 
     static navigationOptions = {
-        title: 'Contact'
+        title: 'Contact Us'
     }
 
     render() {
         return (
             <ScrollView>
-                <Card title="Contact Information" wrapperStyle={{margin: 20}} >
-                    <Text style={{ marginBottom: 10 }} >
-                            1 Nucamp Way
-                            Seattle, WA 98001
-                            U.S.A.
-                    
-                    </Text>
-                    <Text>                        
-                            Phone: 1-206-555-1234
-                            Email: campsites@nucamp.co
-                    </Text>
-
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card
+                        title="Contact Information"
+                        wrapperStyle={{margin: 20}}>
+                        <Text>1 Nucamp Way</Text>
+                        <Text>Seattle, WA 98001</Text>
+                        <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                        <Text>Phone: 1-206-555-1234</Text>
+                        <Text>Email: campsites@nucamp.co</Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
-        )
-    }
+        );
+    };
 }
 
-export default Contact
+export default Contact;
